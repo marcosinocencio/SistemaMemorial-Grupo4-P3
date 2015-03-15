7829326b78f384256bc479f4a50b0176da38a73c
AtmTuma <alexandermgbr@hotmail.com>
2015-03-15 18:50:48 -0300

    sistema memorial 1.1

---
+++ Término dos Ciclos/Implementação/SistemaMemorial - 1.1/src/sistemamemorial/Memorial.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemamemorial;
+
+/**
+ *
+ * @author alex
+ */
+public class Memorial {
+    private String textoUnico;
+    
+    public void criar(String textoUnico){}
+    public void alterar(String textoUnico){}
+}
